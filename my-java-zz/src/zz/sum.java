2cb426707c7147b99d649577969af5601f2157c8
luozhi1024 <13101606873@163.com>
2018-09-14 22:55:19 +0800

    Java

---
+++ my-java-zz/src/zz/sum.java
@@ -0,0 +1,14 @@
+package zz;
+import java.util.Scanner;
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for(int i =1;i<=102;i++){
+			sum = sum +i;
+	}
+		System.out.println(sum);
+
+}
+}
